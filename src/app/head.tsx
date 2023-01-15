f02101a9d5371b7dd18f8c7c164586fc78e49d8e
Olakunle Boye <kunlexzy@gmail.com>
2023-01-15 07:58:22 +0100

    implementing template

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -8,6 +8,13 @@ export default function Head() {
       <meta name="description" content="" />
       <meta name="author" content="" />
       <link rel="icon" href="/favicon.ico" />
+      <link
+        rel="stylesheet"
+        precedence="default" 
+        href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css"
+        integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1"
+        crossOrigin="anonymous"
+      />
     </>
   )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,17 @@
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
-// import styles from './page.module.css'
+import styles from './page.module.css'
+import Navbar from '@/components/navbar/Navbar'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <div>This is home</div>
+    <>
+      <Navbar />
+      <div className={styles.banner_main_bg}>
+        <div className="container"></div>
+      </div>
+    </>
   )
 }
