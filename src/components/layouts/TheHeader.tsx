35c806bfa7fc77bb445acc045392694e0aa847c3
Olakunle Boye <kunlexzy@gmail.com>
2023-01-26 21:31:26 +0100

    feat: create header mobile responsive

--- src/components/layouts/TheHeader.tsx
+++ src/components/layouts/TheHeader.tsx
@@ -6,13 +6,13 @@ export default function Header() {
   return (
     <>
     <nav className="w-full">
-      <div className="container flex justify-between py-5 m-auto">
+      <div className="container flex justify-between lg:px-0 px-4 py-5 m-auto">
         <div className="brand">
           <span className="leading-3 font-semibold text-2xl">Discover</span>
         </div>
-        <div className="search flex justify-between w-4/12">
+        <div className="search lg:relative absolute px-4 left-0 lg:top-0 top-20 flex justify-between w-full lg:w-4/12">
           <div className="w-full">
-            <input type="text" className="bg-transparent text-justify h-full w-full" placeholder="Seach anything"/>
+            <input type="text" className="bg-transparent text-justify h-full w-full pl-3" placeholder="Seach anything"/>
           </div>
           <div className="h-14 w-1/5 ml-3 bg-black flex justify-center items-center rounded-lg">
             <FilterIcon />
