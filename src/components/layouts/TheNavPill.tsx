847491e2f1dccf44c41162e6602f54a1af9e2c06
Olakunle Boye <kunlexzy@gmail.com>
2023-01-26 22:59:23 +0100

    chore: impelementing navpill

--- src/components/layouts/TheNavPill.tsx
+++ src/components/layouts/TheNavPill.tsx
@@ -4,17 +4,31 @@ export default function NavPill() {
   const [items, setItems] = useState([
     'All',
     'Men',
-    'Women'
-  ])
+    'Women',
+    'Kids'
+  ]);
+
+  const [indexItem, setIndex] = useState(0);
+
+  const handleButton = (e: any, index: number) => {
+    e.preventDefault();
+    setIndex(index);
+  }
+
   return (
     <>
-      <div className="w-full">
+      <div className="w-full lg:mt-5 mt-24">
         <div className="container mx-auto">
-          <div className="w-50 flex">
+          <div className="px-4 lg:px-0 flex w-full justify-between lg:justify-start">
             {
               items.map((item, index) => {
                 return (
-                  <button className="bg-black text-white rounded px-4 font-semibold py-1.5 mr-4 text-center" key={index}>{item}</button>
+                  <button
+                    className={`${index === indexItem ? 'bg-black text-white':'bg-[#F2F2F2]'} text-base rounded px-4 font-[400] py-1.5 mr-4 text-center`}
+                    key={index}
+                    onClick={(e) => handleButton(e, index)}>
+                      {item}
+                  </button>
                 )
               })
             }
